5c3f9f0080b4ef4463c96e733c896d97567c2444
Jesse Jafa <jesse.jafa@gmail.com>
2018-08-27 15:15:22 +0300

    dev & prod configs

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports =  {
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(process.cwd(), 'src/index'),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.join(process.cwd(), 'public', 'js'),
+    publicPath: '/js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(s)?css$/, // <-- note this (s)?
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: 'svg-inline-loader',
+      },
+      {
+        test: /\.(jpe?g|png|gif|ico)$/i,
+        use: 'file-loader',
+      },
+    ],
+  },
+  target: 'web',
+};
