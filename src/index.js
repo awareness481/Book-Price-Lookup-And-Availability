83b6120d434de6c2f885f0682346b87fb209cf95
Jesse Jafa <jesse.jafa@gmail.com>
2018-08-28 16:05:44 +0300

    Initial form e.target with stateless components

--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ import styles from './styles/_styles.scss';
 if (module.hot) {
   module.hot.accept();
 }
-const MainApp = () => (
-  <form class='mainForm'>
-    <input id='searchForm' />
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(e.target.children[0].value);
+}
+
+const MainApp = (props) => (
+  <form className='mainForm' onSubmit={handleSubmit}>
+    <input 
+      id='searchForm'
+      value={props.Input}
+    />
   </form>
 );
 // render the app
